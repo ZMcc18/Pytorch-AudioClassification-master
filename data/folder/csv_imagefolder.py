23c66454f0545b313c1e43715bda1a5cd2301d27
ZMcc18 <68259394+ZMcc18@users.noreply.github.com>
2024-01-22 11:39:45 +0800

    Update csv_imagefolder.py

--- data/folder/csv_imagefolder.py
+++ data/folder/csv_imagefolder.py
@@ -7,16 +7,16 @@ A demo for generator DIF(datasets' information file) according to "ImageFolder C
 """
 
 
-def get_class_list(image_dir):
-    return os.listdir(image_dir)
+def get_class_list(image_dir):    # 获取label列表
+    return os.listdir(image_dir)        
 
 
-def generator(csv_path,image_dir):
+def generator(csv_path,image_dir): # 保存到csv文件中
     """
     data items:filename,filepath,label
     """
-    class_list = get_class_list(image_dir)
-    data = {'filename':[], 'filepath':[], 'label':[]}
+    class_list = get_class_list(image_dir)   # 获取label列表
+    data = {'filename':[], 'filepath':[], 'label':[]}  
     for class_name in class_list:
         class_path = image_dir + "/{}".format(class_name)
         for filename in os.listdir(class_path):
